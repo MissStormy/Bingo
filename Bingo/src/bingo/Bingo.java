425b02c32c79c7402af17fbbca9e73939ee6ca61
LilithRelish <laragoblin@outlook.com>
2022-12-16 09:48:27 +0100

    -

--- Bingo/src/bingo/Bingo.java
+++ Bingo/src/bingo/Bingo.java
@@ -1,8 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+// Un asombroso, maravilloso, proyecto de Bingo para navidad
+
 package bingo;
+import java.util.*;
 
 /**
  *
