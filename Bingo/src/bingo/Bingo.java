0ce1115d8b6875153019bccb513818ccbb8a2381
LilithRelish <laragoblin@outlook.com>
2022-12-16 11:55:46 +0100

    menu/funciones

--- Bingo/src/bingo/Bingo.java
+++ Bingo/src/bingo/Bingo.java
@@ -1,7 +1,6 @@
 // Un asombroso, maravilloso, proyecto de Bingo para navidad, para que ganes a tu tia la fascista
-
-
 package bingo;
+
 import java.util.*;
 
 /**
@@ -9,18 +8,37 @@ import java.util.*;
  * @authors Lara & Vera & Tinin
  */
 public class Bingo {
+
     //Funciones
-    
-    public static void main(String[] args) {
-        //Variables
-        Scanner ent = new Scanner(System.in);
-        
+    public static void Menu() {
         System.out.println("Bienvenido al bingo");
         System.out.println("-------------------");
         System.out.println("1. Generar carton");
         System.out.println("2. Mostrar carton");
         System.out.println("3. Sacar bola");
         System.out.println("4. Salir");
+        
     }
-    
+    public static int Gen_Carton(){
+        return 0;
+    }
+    public static void Mos_Carton(){
+        
+    }
+    public static int Bola(){
+        return 0;
+    }
+    //Objetos
+    public static void main(String[] args) {
+        //Variables
+        Scanner ent = new Scanner(System.in);
+        int opc = 0;
+
+        while (opc != 4) {
+            Menu();
+            opc = ent.nextInt();
+        }
+
+    }
+
 }
