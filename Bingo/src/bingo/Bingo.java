00d80bcb7d86fa34cbb960b961f95f72a7716fbb
VeraLoca13 <87098611+VeraLoca13@users.noreply.github.com>
2022-12-19 14:41:07 +0100

    Bingo card generator added
    added part of the code needed to generate bingo card,
    need to finish ordering the numbers before applying them to the card

--- Bingo/src/bingo/Bingo.java
+++ Bingo/src/bingo/Bingo.java
@@ -36,9 +36,55 @@ public class Bingo {
 
     }
 
-    public static int Gen_Carton() {//esto tiene que generar un carton para la abuela, tendriamos que poner alguna comprobacion para
-                                    //que no se pueda sacar mas de uno hasta que no se haya acabado el juego
-        return 0;
+    public static int[][] Gen_Carton(int[][] carton) {//esto tiene que generar un carton para la abuela, tendriamos que poner alguna comprobacion para
+                                                      //que no se pueda sacar mas de uno hasta que no se haya acabado el juego
+        int[][] bloqueadas = new int [3][9];
+        for (int i = 0; i < bloqueadas.length; i++) {
+            for (int j = 0; j < 4;) {//Generamos las 4 casillas que estaran bloaqueadas en cada linea, con cuidado al crear la tercera linea de que no haya ninguna columna entera bloqueada
+                int casilla = (int) (Math.random()*9);
+                if (bloqueadas[i][casilla]==0 && (bloqueadas[1][casilla] == 0 | bloqueadas[2][casilla] == 0)){
+                    bloqueadas[i][casilla]=1;
+                    i++;
+                }
+            }
+        }
+        for (int i = 0; i < carton.length; i++) {   //Generamos los numeros que se pondran en las casillas libres y los establecemos
+            int[] columna = {10,10,10};
+            if (i>0){                               //generamos los numeros de la columna
+                for (int j = 0; j < columna.length;) {
+                    int num = (int) (Math.random()*9);
+                    if (num != columna[0] && num != columna[1] && num != columna[2]){
+                        columna[j]=num;
+                    }
+                }
+                }else{
+                    for (int j = 0; j < columna.length;) {
+                    int num = (int) (Math.random()*8)+1;
+                    if (num != columna[0] && num != columna[1] && num != columna[2]){
+                        columna[j]=num;
+                    }
+                }
+            }
+                int[] pos = new int [columna.length]; //Ordenamos los numeros de la columna de menor a mayor
+                for (int j = 0; j < columna.length; j++){
+                    pos[j]=0;
+                    for(int k = 0; k < columna.length; k++){
+                        if ((k != j) && (columna[k]<columna[j])){
+                            pos[j]++;
+                        }
+                    }
+                }
+                int temp;
+                for (int j = 0; j < columna.length; j++) {
+                    //to do: ACABAR ESTA MIERDA
+                }
+                for (int j = 0; j < carton[i].length; j++) {
+                    if (bloqueadas[i][j]==0){
+                        carton[i][j]=columna[j]+i*10;
+                    }
+                }
+        }
+        return null;
     }
 
     public static void Mos_Carton() {//mostramos el carton con los numeros que ya nos han salido y los que no, buscar como hacerlo con JAVAFX
@@ -77,6 +123,7 @@ public class Bingo {
         
         int[] bolas;
         int opc = 0;
+        int[][] carton1 = new int [3][9];
         Saludo(); //esto solo aparece una vez al principio
         
         while (opc != 4) {
@@ -84,7 +131,7 @@ public class Bingo {
             opc = ent.nextInt();
             switch(opc){
                 case 1:
-                    Gen_Carton();
+                    Gen_Carton(carton1);
                     break;
                 case 2:
                     Mos_Carton();
