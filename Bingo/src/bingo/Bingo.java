2df24e58678c9b01be36a749da7134bb2f5c4014
LilithRelish <laragoblin@outlook.com>
2023-01-09 13:34:36 +0100

    TERMINAO
    Ya no da ni errores, somos diosas :)

--- Bingo/src/bingo/Bingo.java
+++ Bingo/src/bingo/Bingo.java
@@ -34,88 +34,97 @@ public class Bingo {
     }
 
     public static int[][] Gen_Carton(int[][] carton) {//esto tiene que generar un carton para la abuela, tendriamos que poner alguna comprobacion para
-        //que no se pueda sacar mas de uno hasta que no se haya acabado el juego
-        for (int i = 0; i < carton.length; i++) { //borrar carton antes de regenerarlo
-            for (int j = 0; j < carton[i].length; j++) {
-                carton[i][j] = 0;
-            }
-        }
-        int[][] bloqueadas = new int[3][9];
-        for (int i = 0; i < bloqueadas.length; i++) {
-            for (int j = 0; j < 4;) {//Generamos las 4 casillas que estaran bloaqueadas en cada linea, con cuidado al crear la tercera linea de que no haya ninguna columna entera bloqueada
-                int casilla = (int) (Math.random() * 9);
-                if (bloqueadas[i][casilla] == 0 && (bloqueadas[1][casilla] == 0 || bloqueadas[2][casilla] == 0)) {
-                    bloqueadas[i][casilla] = 1;
-                    j++;
+        try {
+            //que no se pueda sacar mas de uno hasta que no se haya acabado el juego
+            for (int i = 0; i < carton.length; i++) { //borrar carton antes de regenerarlo
+                for (int j = 0; j < carton[i].length; j++) {
+                    carton[i][j] = 0;
                 }
             }
-        }
-        for (int i = 0; i < 9; i++) {   //Generamos los numeros que se pondran en las casillas libres y los establecemos
-            int[] columna = {10, 10, 10};
-            if (i > 0) {                               //generamos los numeros de la columna
-                for (int j = 0; j < columna.length;) {
-                    int num = (int) (Math.random() * 9);
-                    if (num != columna[0] && num != columna[1] && num != columna[2]) { //por alguna razon genera columnas bloqueadas enteras, habria que mirarlo o algo supongo
-                        columna[j] = num;
-                        j++;
-                    }
-                }
-            } else {
-                for (int j = 0; j < columna.length;) {
-                    int num = (int) (Math.random() * 8) + 1;
-                    if (num != columna[0] && num != columna[1] && num != columna[2]) {
-                        columna[j] = num;
+            int[][] bloqueadas = new int[3][9];
+            for (int i = 0; i < bloqueadas.length; i++) {
+                for (int j = 0; j < 4;) {//Generamos las 4 casillas que estaran bloaqueadas en cada linea, con cuidado al crear la tercera linea de que no haya ninguna columna entera bloqueada
+                    int casilla = (int) (Math.random() * 9);
+                    if (bloqueadas[i][casilla] == 0 && (bloqueadas[1][casilla] == 0 || bloqueadas[2][casilla] == 0)) {
+                        bloqueadas[i][casilla] = 1;
                         j++;
                     }
                 }
             }
-            for (int j = 0; j < columna.length;) { //Ordenamos los numeros de la columna de menor a mayor
-                int pos = 0;
-                for (int k = 0; k < columna.length; k++) {
-                    if (columna[j] > columna[k]) {
-                        pos++;
+            for (int i = 0; i < 9; i++) {   //Generamos los numeros que se pondran en las casillas libres y los establecemos
+                int[] columna = {10, 10, 10};
+                if (i > 0) {                               //generamos los numeros de la columna
+                    for (int j = 0; j < columna.length;) {
+                        int num = (int) (Math.random() * 9);
+                        if (num != columna[0] && num != columna[1] && num != columna[2]) { //por alguna razon genera columnas bloqueadas enteras, habria que mirarlo o algo supongo
+                            columna[j] = num;
+                            j++;
+                        }
+                    }
+                } else {
+                    for (int j = 0; j < columna.length;) {
+                        int num = (int) (Math.random() * 8) + 1;
+                        if (num != columna[0] && num != columna[1] && num != columna[2]) {
+                            columna[j] = num;
+                            j++;
+                        }
                     }
                 }
-                if (pos > j) {
-                    int temp = columna[pos];
-                    columna[pos] = columna[j];
-                    columna[j] = temp;
-                } else if (pos == j) {
-                    j++;
+                for (int j = 0; j < columna.length;) { //Ordenamos los numeros de la columna de menor a mayor
+                    int pos = 0;
+                    for (int k = 0; k < columna.length; k++) {
+                        if (columna[j] > columna[k]) {
+                            pos++;
+                        }
+                    }
+                    if (pos > j) {
+                        int temp = columna[pos];
+                        columna[pos] = columna[j];
+                        columna[j] = temp;
+                    } else if (pos == j) {
+                        j++;
+                    }
+                }
+                for (int j = 0; j < 3; j++) {
+                    if (bloqueadas[j][i] == 0) {
+                        carton[j][i] = columna[j] + i * 10;
+                    }
                 }
             }
-            for (int j = 0; j < 3; j++) {
-                if (bloqueadas[j][i] == 0) {
-                    carton[j][i] = columna[j] + i * 10;
-                }
-            }
+
+        } catch (Exception e) {
+            System.out.println("Problema detectado, cerrando programa.");
         }
         return carton;
     }
 
     public static void Mos_Carton(int[][] carton, int[] bolas) {//mostramos el carton con los numeros que ya nos han salido y los que no, buscar como hacerlo con JAVAFX
-        boolean bingo = true;
-        boolean linea = true;
-        for (int i = 0; i < carton.length; i++) {
-            linea = true;
-            for (int j = 0; j < carton[i].length; j++) {
-                if (carton[i][j] == 0) {
-                    System.out.print(WHITE + "| " + CYAN + "##" + WHITE + " |");
-                } else if (carton[i][j] == 100) {
-                    System.out.print(WHITE + "| " + PURPLE + "##" + WHITE + " |");
-                } else {
-                    System.out.print(WHITE + "| " + carton[i][j] + " |");
-                    linea = false;
-                    bingo = false;
+        try {
+            boolean bingo = true;
+            boolean linea = true;
+            for (int i = 0; i < carton.length; i++) {
+                linea = true;
+                for (int j = 0; j < carton[i].length; j++) {
+                    if (carton[i][j] == 0) {
+                        System.out.print(WHITE + "| " + CYAN + "##" + WHITE + " |");
+                    } else if (carton[i][j] == 100) {
+                        System.out.print(WHITE + "| " + PURPLE + "##" + WHITE + " |");
+                    } else {
+                        System.out.print(WHITE + "| " + carton[i][j] + " |");
+                        linea = false;
+                        bingo = false;
+                    }
                 }
+                if (linea) {
+                    System.out.print("  ¡¡LINEA!!");
+                }
+                System.out.println("");
             }
-            if (linea) {
-                System.out.print("  ¡¡LINEA!!");
+            if (bingo) {
+                System.out.println("          ¡¡¡BINGO!!!");
             }
-            System.out.println("");
-        }
-        if (bingo) {
-            System.out.println("          ¡¡¡BINGO!!!");
+        } catch (Exception e) {
+            System.out.println("Error detectado, cerrando programa");
         }
 
     }
@@ -124,53 +133,60 @@ public class Bingo {
        
     }*/
     public static int Bola(int bola, int[] bolas, int b, int[][] carton, int bolas_sacadas) {//sacamos la bola y la cantamos como los niños de san ildefonso
-        boolean quedan = false;
-        for (int i = 0; i < bolas.length; i++) {
-            if (bolas[i] != 0) {
-                quedan = true;
-                i = bolas.length;
-            }
-        }
-        if (quedan) {
-            bolas_sacadas++;
-            System.out.println("El bombo esta girando...");
-            System.out.println("La bola esta saliendo...");
-            
-            boolean vanpiro_esiten = false;
-            while (!vanpiro_esiten){
-            for (b = 0; b < 89; b++) { //recorremos el array de bolas posibles
-                if (bola == bolas[b]) {//si la bola coincide con alguna de las opciones, pone un 0 para mostrar que ese numero ha salido
-                    bolas[b] = 0;
-                    vanpiro_esiten=true;
-                    //return bolas[b];
+        try {
+            boolean quedan = false;
+            for (int i = 0; i < bolas.length; i++) {
+                if (bolas[i] != 0) {
+                    quedan = true;
+                    i = bolas.length;
                 }
             }
-            bola++;
-            if (bola >89)bola=1;
-            }
+            if (quedan) {
+                bolas_sacadas++;
+                System.out.println("El bombo esta girando...");
+                System.out.println("La bola esta saliendo...");
 
-            System.out.println("La bola es: " + (bola-1));
+                boolean vanpiro_esiten = false;
+                while (!vanpiro_esiten) {
+                    for (b = 0; b < 89; b++) { //recorremos el array de bolas posibles
+                        if (bola == bolas[b]) {//si la bola coincide con alguna de las opciones, pone un 0 para mostrar que ese numero ha salido
+                            bolas[b] = 0;
+                            vanpiro_esiten = true;
+                            //return bolas[b];
+                        }
+                    }
+                    bola++;
+                    if (bola > 89) {
+                        bola = 1;
+                    }
+                }
 
-            for (int i = 0; i < carton.length; i++) {
-                for (int j = 0; j < carton[i].length; j++) {
-                    for (int k = 0; k < bolas.length; k++) {
-                        if (carton[i][j] != 100 && carton[i][j] != 0) {
-                            if (bolas[(carton[i][j] - 1)] == 0) {
-                                carton[i][j] = 100;
+                System.out.println("La bola es: " + (bola - 1));
+
+                for (int i = 0; i < carton.length; i++) {
+                    for (int j = 0; j < carton[i].length; j++) {
+                        for (int k = 0; k < bolas.length; k++) {
+                            if (carton[i][j] != 100 && carton[i][j] != 0) {
+                                if (bolas[(carton[i][j] - 1)] == 0) {
+                                    carton[i][j] = 100;
+                                }
                             }
                         }
                     }
                 }
-            }
-            //for (b = 0; b < 89; b++) {
-            //    System.out.println(bolas[b]);
-            //}
+                //for (b = 0; b < 89; b++) {
+                //    System.out.println(bolas[b]);
+                //}
 
-            
-        }else System.out.println("No quedan mas bolas");
+            } else {
+                System.out.println("No quedan mas bolas");
+            }
+        } catch (Exception e) {
+            System.out.println("Error detectado, cerrando programa");
+        }
         return bolas_sacadas;
     }
-    
+
     public static void Mostrar_Num(int[] bolas) {
         for (int i = 0; i < 89; i++) {
             System.out.println(bolas[i]);
@@ -194,48 +210,52 @@ public class Bingo {
 
     //Objetos
     public static void main(String[] args) {
-        //Variables
-        Scanner ent = new Scanner(System.in);
-        Random rand = new Random();
-        int bolas_sacadas = 0;
+        try {
+            //Variables
+            Scanner ent = new Scanner(System.in);
+            Random rand = new Random();
+            int bolas_sacadas = 0;
 
-        int[] bolas = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44,
-            45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89};
-        int opc = 0;
-        int i;
-        int bola;
-        int[][] carton1 = new int[3][9];
-        Saludo(); //esto solo aparece una vez al principio
+            int[] bolas = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44,
+                45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89};
+            int opc = 0;
+            int i;
+            int bola;
+            int[][] carton1 = new int[3][9];
+            Saludo(); //esto solo aparece una vez al principio
 
-        while (opc != 4) {
-            Menu();
-            opc = ent.nextInt();
-            switch (opc) {
-                case 1:
-                    Gen_Carton(carton1);
-                    break;
-                case 2:
-                    Mos_Carton(carton1, bolas);
-                    break;
-                case 3:
-                    int b = 0;
-                    bola = rand.nextInt(88) + 1;
-                    bolas_sacadas = Bola(bola, bolas, b, carton1, bolas_sacadas);
+            while (opc != 4) {
+                Menu();
+                opc = ent.nextInt();
+                switch (opc) {
+                    case 1:
+                        Gen_Carton(carton1);
+                        break;
+                    case 2:
+                        Mos_Carton(carton1, bolas);
+                        break;
+                    case 3:
+                        int b = 0;
+                        bola = rand.nextInt(88) + 1;
+                        bolas_sacadas = Bola(bola, bolas, b, carton1, bolas_sacadas);
 
-                    break;
-                case 4:
-                    break;
-                case 5:
-                    Mostrar_Num(bolas);
-                    break;
-                case 7:
-                    System.out.println(bolas_sacadas);
-                    break;
-                default:
-                    break;
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        Mostrar_Num(bolas);
+                        break;
+                    case 7:
+                        System.out.println(bolas_sacadas);
+                        break;
+                    default:
+                        break;
+                }
             }
-        }
 
+        } catch (Exception e) {
+            System.out.println("Error detectado, cerrando programa");
+        }
     }
 
 }
